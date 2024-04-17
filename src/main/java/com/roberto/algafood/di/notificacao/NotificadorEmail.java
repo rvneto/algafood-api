983c1c55f5e59d08ea6625cfe46480046432c54c
roberto <rvneto.rs@gmail.com>
2024-04-16 21:53:40 -0300

    Ambiguidade de beans com @Primary

--- src/main/java/com/roberto/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/roberto/algafood/di/notificacao/NotificadorEmail.java
@@ -1,8 +1,10 @@
 package com.roberto.algafood.di.notificacao;
 
 import com.roberto.algafood.di.modelo.Cliente;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 public class NotificadorEmail implements Notificador {
 
--- src/main/java/com/roberto/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/roberto/algafood/di/service/AtivacaoClienteService.java
@@ -5,20 +5,14 @@ import com.roberto.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class AtivacaoClienteService {
 
     @Autowired
-    private List<Notificador> notificadores;
+    private Notificador notificador;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-
-        for (Notificador notificador : notificadores) {
-            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-        }
-
+        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
 }
