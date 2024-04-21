eeee3e53d4580bd78fd4ce0c932e26cb5b6df70b
roberto <rvneto.rs@gmail.com>
2024-04-21 20:24:16 -0300

    Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/roberto/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/roberto/algafood/di/notificacao/NotificadorEmail.java
@@ -1,14 +1,22 @@
 package com.roberto.algafood.di.notificacao;
 
 import com.roberto.algafood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @TipoDoNotificador(NivelUrgencia.NORMAL)
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+    @Value("${notificador.email.porta-servidor}")
+    private Integer port;
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + host);
+        System.out.println("Port: " + port);
         System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 
--- src/main/java/com/roberto/algafood/listener/NotificacaoService.java
+++ src/main/java/com/roberto/algafood/listener/NotificacaoService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 public class NotificacaoService {
 
     @Autowired
-    @TipoDoNotificador(NivelUrgencia.URGENTE)
+    @TipoDoNotificador(NivelUrgencia.NORMAL)
     private Notificador notificador;
 
     @EventListener
