7125c3b1b496fb040e3d01c8eaa1d51b25c8a718
roberto <rvneto.rs@gmail.com>
2024-04-21 20:03:27 -0300

    Publicando e consumindo eventos customizados

--- src/main/java/com/roberto/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/roberto/algafood/di/service/AtivacaoClienteService.java
@@ -1,21 +1,18 @@
 package com.roberto.algafood.di.service;
 
 import com.roberto.algafood.di.modelo.Cliente;
-import com.roberto.algafood.di.notificacao.NivelUrgencia;
-import com.roberto.algafood.di.notificacao.Notificador;
-import com.roberto.algafood.di.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
     @Autowired
-    @TipoDoNotificador(NivelUrgencia.URGENTE)
-    private Notificador notificador;
+    private ApplicationEventPublisher eventPublisher;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
     }
 }
---
+++ src/main/java/com/roberto/algafood/listener/NotificacaoService.java
@@ -0,0 +1,23 @@
+package com.roberto.algafood.listener;
+
+import com.roberto.algafood.di.notificacao.NivelUrgencia;
+import com.roberto.algafood.di.notificacao.Notificador;
+import com.roberto.algafood.di.notificacao.TipoDoNotificador;
+import com.roberto.algafood.di.service.ClienteAtivadoEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificacaoService {
+
+    @Autowired
+    @TipoDoNotificador(NivelUrgencia.URGENTE)
+    private Notificador notificador;
+
+    @EventListener
+    public void clienteAtivadoListener(ClienteAtivadoEvent event) {
+        notificador.notificar(event.getCliente(), "Seu cadastro no sistema está ativo!");
+    }
+
+}
