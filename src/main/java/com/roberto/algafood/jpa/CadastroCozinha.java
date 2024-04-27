2a3131cf61e17aff63e0eb4c4353c68d9bb23d50
roberto <rvneto.rs@gmail.com>
2024-04-27 12:33:11 -0300

    Consultando objetos do banco de dados

---
+++ src/main/java/com/roberto/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,21 @@
+package com.roberto.algafood.jpa;
+
+import com.roberto.algafood.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class CadastroCozinha {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Cozinha> listar() {
+        return entityManager.createQuery("from Cozinha", Cozinha.class)
+                .getResultList();
+    }
+
+}
