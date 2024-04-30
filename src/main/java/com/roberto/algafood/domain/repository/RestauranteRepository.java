70df8b5278235172967f1b0d47e610060c9b986d
roberto <rvneto.rs@gmail.com>
2024-04-29 21:56:44 -0300

    Usando queries JPQL customizadas com @Query

--- src/main/java/com/roberto/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/roberto/algafood/domain/repository/RestauranteRepository.java
@@ -2,6 +2,8 @@ package com.roberto.algafood.domain.repository;
 
 import com.roberto.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -19,5 +21,8 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
     List<Restaurante> findTop2ByNomeContaining(String nome);
 
     int countByCozinhaId(Long cozinhaId);
+
+    @Query("from Restaurante where nom like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
 
 }
