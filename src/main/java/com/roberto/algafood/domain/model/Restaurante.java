8ea7174867f391e58f80041a0f616e699ce4e44c
roberto <rvneto.rs@gmail.com>
2024-05-02 19:52:45 -0300

    Alterando a estratégia de fetching para Eager Loading

--- src/main/java/com/roberto/algafood/domain/model/Restaurante.java
+++ src/main/java/com/roberto/algafood/domain/model/Restaurante.java
@@ -49,7 +49,7 @@ public class Restaurante {
     private Endereco endereco;
 
     @JsonIgnore
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "restaurante_forma_pagamento",
             joinColumns = @JoinColumn(name = "restaurante_id"),
             inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
