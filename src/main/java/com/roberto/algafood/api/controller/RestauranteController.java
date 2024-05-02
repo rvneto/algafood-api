aacf6450aaf7292c4ac71a95b9bc5b91961690e7
roberto <rvneto.rs@gmail.com>
2024-05-02 18:25:37 -0300

    Testando e analisando o impacto da incorporação de classe na REST API

--- src/main/java/com/roberto/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/roberto/algafood/api/controller/RestauranteController.java
@@ -58,7 +58,7 @@ public class RestauranteController {
         try {
             Restaurante restauranteAtual = restauranteRepository.findById(id).orElse(null);
             if (restauranteAtual != null) {
-                BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento");
+                BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco");
                 cadastroRestauranteService.salvar(restauranteAtual);
                 return ResponseEntity.ok(restauranteAtual);
             }
