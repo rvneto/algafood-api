eae0f9a0d3325a2031cfc8019d5fabbafe37ad13
roberto <rvneto.rs@gmail.com>
2024-05-04 18:16:59 -0300

    Tratando exceções em nível de controlador com @ExceptionHandler

---
+++ src/main/java/com/roberto/algafood/api/exceptionhandler/Problema.java
@@ -0,0 +1,15 @@
+package com.roberto.algafood.api.exceptionhandler;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class Problema {
+
+    private LocalDateTime dataHora;
+    private String mensagem;
+
+}
