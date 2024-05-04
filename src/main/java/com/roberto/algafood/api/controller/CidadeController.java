9c02ad0978d7829163ca5010c4f48db90a3dee3a
roberto <rvneto.rs@gmail.com>
2024-05-04 18:30:06 -0300

    Tratando exceções globais com @ExceptionHandler e @ControllerAdvice

--- src/main/java/com/roberto/algafood/api/controller/CidadeController.java
+++ src/main/java/com/roberto/algafood/api/controller/CidadeController.java
@@ -1,7 +1,5 @@
 package com.roberto.algafood.api.controller;
 
-import com.roberto.algafood.api.exceptionhandler.Problema;
-import com.roberto.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.roberto.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.roberto.algafood.domain.exception.NegocioException;
 import com.roberto.algafood.domain.model.Cidade;
@@ -10,10 +8,8 @@ import com.roberto.algafood.domain.service.CadastroCidadeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -66,15 +62,6 @@ public class CidadeController {
         cadastroCidade.excluir(cidadeId);
     }
 
-    @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> tratarEntidadeNaoEncotradoException(EntidadeNaoEncontradaException e) {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(problema);
-    }
 
 }
---
+++ src/main/java/com/roberto/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.roberto.algafood.api.exceptionhandler;
+
+import com.roberto.algafood.domain.exception.EntidadeNaoEncontradaException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<?> tratarEntidadeNaoEncotradoException(EntidadeNaoEncontradaException e) {
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(problema);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity tratarHttpMediaTypeNotSupportedException() {
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem("O tipo de mídia não é aceito.")
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                .body(problema);
+    }
+}
